4a298502c858ba835279f2c885956eaf20c214a0
Mahmoud Aboeleneen <aboeleneeen.98@yahoo.com>
2021-06-11 16:24:10 +0200

    Implement RequestGenerator class to generate sequence of operations

--- RMIClient/src/rmi/client/GraphManipulation.java
+++ RMIClient/src/rmi/client/GraphManipulation.java
@@ -3,26 +3,23 @@ package rmi.client;
 import java.io.File;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 import rmi.registery.GraphService;
 
 public class GraphManipulation {
 	public static void main(String args[]) {
 		try {
-			String name = "GraphService";
-			Registry registry = LocateRegistry.getRegistry("localhost");
-			GraphService graphService = (GraphService) registry.lookup(name);
-			String batch = new Scanner(new File("batch_1.txt")).useDelimiter("\\Z").next();
-			System.out.println(batch);
-			System.out.println("get the graph before edit");
-			System.out.println(graphService.getCurrentGraph());
-			System.out.println("perform operations");
-			System.out.println(graphService.excuteBatchOperations(batch));
-			System.out.println("get the graph after edit");
-			System.out.println(graphService.getCurrentGraph());
+//			String name = "GraphService";
+//			Registry registry = LocateRegistry.getRegistry("localhost");
+//			GraphService graphService = (GraphService) registry.lookup(name);
+			RequestGenerator generator = new RequestGenerator(0.3, 10 , 20);
+			System.out.println(generator.getReqeust());
 		} catch (Exception e) {
 			System.err.println("GraphService exception:");
 			e.printStackTrace();
 		}
 	}
+
 }
---
+++ RMIClient/src/rmi/client/RequestGenerator.java
@@ -0,0 +1,71 @@
+package rmi.client;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class RequestGenerator {
+	private ArrayList<String> operations ; 
+	private double writingPercentage ;
+	private int numOfQueries;
+	private int numOfNodes ;
+	Random nodeRandomGenerator ;
+	Random operRandomGenerator ;
+	public RequestGenerator(double writingPercentage , int numOfQueries , int numOfNodes) {
+		 this.operations = new ArrayList<String>();
+		 this.nodeRandomGenerator = new Random();
+		 this.operRandomGenerator = new Random();
+		 this.writingPercentage = writingPercentage;
+		 this.numOfQueries = numOfQueries;
+		 this.numOfNodes = numOfNodes;
+	}
+	
+	public void setWritingPercentage(double writingPercentage) {
+		this.writingPercentage = writingPercentage;
+	}
+	
+	public void setNumOfQueries(int numOfQueries) {
+		this.numOfQueries = numOfQueries;
+	}
+	public String getReqeust() {
+		constructRequest();
+		String parsedRequest = "" ;
+		for(String oper : operations) {
+			parsedRequest+=oper;
+			parsedRequest+="\n";
+		}
+		parsedRequest+="F";
+		return parsedRequest;
+	}
+	
+	private  void constructRequest() {
+		this.operations = new ArrayList<String>();
+		for(int i=0;i<numOfQueries;i++) {
+			if(i < numOfQueries*writingPercentage) {
+				operations.add(generateWriteOperation());
+			}else {
+				operations.add(generateReadOperation());
+			}
+		}
+        Collections.shuffle(operations);
+	}
+	
+	private String generateTwoNodes() {
+		int node1 = nodeRandomGenerator.nextInt(numOfNodes)+1;
+		int node2 = nodeRandomGenerator.nextInt(numOfNodes)+1;
+		return node1 + " " + node2 ;
+	}
+	
+	private String generateWriteOperation() {
+		int addOperation = operRandomGenerator.nextInt(2);
+		if(addOperation == 0) {
+			return "A " + generateTwoNodes();
+		}else {
+			return "D " + generateTwoNodes();
+		}
+	}
+	
+	private String generateReadOperation() {
+		return "Q " + generateTwoNodes();
+	}
+}
